2a9f7a0f7cab61ea1284445cda5de02edf77670d
Xicheng Guo <xicheng_guo@wistron.com>
2026-02-07 18:01:24 +0800

    update

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import OCRComponent from '../components/OCRComponent.vue'
-import OCRComponent2 from '../components/OCRComponent2.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
@@ -10,7 +9,6 @@ export default {
     ...DefaultTheme,
     enhanceApp({ app }) {
         app.component('OCRComponent', OCRComponent);
-        app.component('OCRComponent2', OCRComponent2);
         app.use(ElementPlus);
     }
 }
